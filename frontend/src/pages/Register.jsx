f9d64046b9330f9c2b40d40d5812f7f8effe57c0
Dhanunjay Sri Sai Jampa <149061010+dhanunjaysrisai@users.noreply.github.com>
2026-04-17 20:43:55 +0530

    Update image and video sources to use URL constructor

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -72,10 +72,10 @@ function Register() {
       <nav className="navbar">
         <div className="navbar__content">
           <div className="navbar__left">
-            <img src="../images/slogon logo.png" alt="Sssssssssssssssss" className="slogan-img" />
+            <img src={new URL('../images/slogon logo.png', import.meta.url).href} alt="SwachhMitra Slogan" className="slogan-img" />
           </div>
           <div className="navbar__right">
-            <img src="../images/swatchhmita logo.png" alt="SwachhMitra Logo" className="brand-img" />
+            <img src={new URL('../images/swatchhmita logo.png', import.meta.url).href} alt="SwachhMitra Logo" className="brand-img" />
           </div>
         </div>
       </nav>
@@ -167,7 +167,7 @@ function Register() {
           <div className="auth-media">
             <video
               className="auth-video"
-              src="../videos/onesteplogo.mp4"
+              src={new URL('../videos/onesteplogo.mp4', import.meta.url).href}
               autoPlay
               muted
               loop
